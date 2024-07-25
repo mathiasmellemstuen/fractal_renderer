a367174fd77f40ec1e9341068fc85f92e3048da4
Mathias Mellemstuen <mathias.mellemstuen@gmail.com>
2024-07-25 21:53:03 +0200

    Adding indicantif progress.

--- src/mandelbrot.rs
+++ src/mandelbrot.rs
@@ -1,16 +1,16 @@
-use ndarray::prelude::*; 
+use ndarray::prelude::*;
 
 pub fn create_mandelbrot_frame_image(max_iterations : usize, x_size : usize, y_size : usize, x_pos : f64, y_pos : f64, radius : f64, color_gradient : &colorgrad::Gradient, color_gradient_shift : f64) -> Array3::<u8> {
-
-    let mut buffer = Array3::<u8>::zeros((y_size, x_size, 3));
 
     let aspect : f64 = x_size as f64 / y_size as f64; 
     let x_radius = radius; 
     let y_radius = radius / aspect; 
+
     
-    for x in 0 .. x_size {
-        for y in 0 .. y_size {
-            
+    let mut buffer = Array3::<u8>::zeros((y_size, x_size, 3));
+
+    buffer.axis_iter_mut(Axis(0)).enumerate().for_each(|(y, mut column)| {
+        column.axis_iter_mut(Axis(0)).enumerate().for_each(|(x, mut channels)| {
             let x_0 : f64 = (x_pos - x_radius) + (x as f64 / x_size as f64) * ((x_pos + x_radius) - (x_pos - x_radius));
             let y_0 : f64 = (y_pos - y_radius) + (y as f64 / y_size as f64) * ((y_pos + y_radius) - (y_pos - y_radius));
             
@@ -26,19 +26,16 @@ pub fn create_mandelbrot_frame_image(max_iterations : usize, x_size : usize, y_s
 
                 iteration += 1; 
             }
+            let mut value = (iteration as f64 / max_iterations as f64) + color_gradient_shift; 
+            value = value - value.floor();
+            
+            let g_value = color_gradient.at(value).to_rgba8(); 
 
-            let mut g_value = ((iteration as f64) / (max_iterations as f64)) + color_gradient_shift;
-
-            // We need to do this because we are shifting the value in the previous line. This is to
-            // keep the final value between 0 - 1. 
-            g_value = g_value - g_value.floor(); 
-
-            let color = color_gradient.at(g_value).to_rgba8();
-            buffer[[y, x, 0]] = color[0];
-            buffer[[y, x, 1]] = color[1];
-            buffer[[y, x, 2]] = color[2];
-        }
-    }
+            channels[0] = g_value[0]; 
+            channels[1] = g_value[1]; 
+            channels[2] = g_value[2]; 
+        });
+    });
 
     buffer
 }
--- src/mp4.rs
+++ src/mp4.rs
@@ -4,6 +4,8 @@ use video_rs::encode::{Encoder, Settings};
 use video_rs::time::Time;
 use std::path::Path;
 use indicatif::ProgressBar;
+use indicatif::ProgressStyle;
+use std::time::Instant;
 
 pub fn create_mp4(x_size : usize, y_size : usize, frames_per_second : usize, frames : &Vec<FrameMeta>, color_gradient : &colorgrad::Gradient) {
     let settings = Settings::preset_h264_yuv420p(x_size, y_size, false);
@@ -13,15 +15,24 @@ pub fn create_mp4(x_size : usize, y_size : usize, frames_per_second : usize, fra
     let mut position = Time::zero();
     
     let progress_bar = ProgressBar::new(frames.len() as u64); 
+    progress_bar.set_style(
+        ProgressStyle::with_template(
+            "{spinner:.green} [{elapsed_precise}] [{bar:40.cyan/blue}] ({pos}/{len}, FPS {per_sec}, ETA {eta})",
+        )
+        .unwrap(),
+    );
     
     for f in frames.iter() {
         
+        let now = Instant::now(); 
         let frame = create_mandelbrot_frame_image(f.max_iterations, x_size, y_size, f.x_pos, f.y_pos, f.radius, &color_gradient, f.color_gradient_shift);
 
         encoder.encode(&frame, position).expect("Failed to encode video!");
 
         position = position.aligned_with(duration).add(); 
 
+        let elapsed = now.elapsed().as_millis();
+        // println!("{} ms", elapsed); 
         progress_bar.inc(1); 
     }
 
