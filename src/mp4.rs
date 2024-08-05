ee9bd1a5734141860699dd7d896c24fcfdbf8659
Mathias Mellemstuen <mathias.mellemstuen@gmail.com>
2024-08-05 13:01:37 +0200

    Minor changes.

--- src/mp4.rs
+++ src/mp4.rs
@@ -9,7 +9,7 @@ use crate::fractal_function_type::FractalFunctionType;
 
 pub fn create_mp4(x_size : usize, y_size : usize, frames_per_second : usize, frames : &Vec<FrameMeta>, color_gradient : &colorgrad::Gradient, fractal_f : FractalFunctionType) {
     let settings = Settings::preset_h264_yuv420p(x_size, y_size, false);
-    let mut encoder = Encoder::new(Path::new("mandelbrot.mp4"), settings).expect("Failed to create encoder");
+    let mut encoder = Encoder::new(Path::new("video.mp4"), settings).expect("Failed to create encoder");
 
     let duration: Time = Time::from_nth_of_a_second(frames_per_second);
     let mut position = Time::zero();
@@ -26,7 +26,7 @@ pub fn create_mp4(x_size : usize, y_size : usize, frames_per_second : usize, fra
         
         let frame = create_frame_image_par(f.max_iterations, x_size, y_size, f.x_pos, f.y_pos, f.radius, &color_gradient, f.color_gradient_shift, fractal_f);
 
-        encoder.encode(&frame, position).expect("Failed to encode video!");
+        encoder.encode(&frame, position).expect("Failed to encode video");
 
         position = position.aligned_with(duration).add(); 
 
